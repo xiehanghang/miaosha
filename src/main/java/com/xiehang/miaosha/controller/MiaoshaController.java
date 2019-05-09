5e5f3ed386a97cc0bc51ee13a29a2bf2ef3daac5
xiehang <1309049396@qq.com>
2019-05-09 13:31:42 +0800

    秒杀页面缓存

--- src/main/java/com/xiehang/miaosha/controller/MiaoshaController.java
+++ src/main/java/com/xiehang/miaosha/controller/MiaoshaController.java
@@ -1,9 +1,11 @@
 package com.xiehang.miaosha.controller;
 
+import com.xiehang.miaosha.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.xiehang.miaosha.domain.MiaoshaOrder;
@@ -16,6 +18,7 @@ import com.xiehang.miaosha.service.MiaoshaService;
 import com.xiehang.miaosha.service.MiaoshaUserService;
 import com.xiehang.miaosha.service.OrderService;
 import com.xiehang.miaosha.vo.GoodsVo;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/miaosha")
@@ -35,31 +38,35 @@ public class MiaoshaController {
 	
 	@Autowired
 	MiaoshaService miaoshaService;
-	
-    @RequestMapping("/do_miaosha")
-    public String list(Model model,MiaoshaUser user,
-    		@RequestParam("goodsId")long goodsId) {
-    	model.addAttribute("user", user);
-    	if(user == null) {
-    		return "login";
-    	}
-    	//判断库存
-    	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
-    	int stock = goods.getStockCount();
-    	if(stock <= 0) {
-    		model.addAttribute("errmsg", CodeMsg.MIAO_SHA_OVER.getMsg());
-    		return "miaosha_fail";
-    	}
-    	//判断是否已经秒杀到了
-    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
-    	if(order != null) {
-    		model.addAttribute("errmsg", CodeMsg.REPEATE_MIAOSHA.getMsg());
-    		return "miaosha_fail";
-    	}
-    	//减库存 下订单 写入秒杀订单
-    	OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
-    	model.addAttribute("orderInfo", orderInfo);
-    	model.addAttribute("goods", goods);
-        return "order_detail";
-    }
+
+	/**
+	 * QPS:1306
+	 * 5000 * 10
+	 * */
+	/**
+	 *  GET POST有什么区别？
+	 * */
+	@RequestMapping(value="/do_miaosha", method= RequestMethod.POST)
+	@ResponseBody
+	public Result<OrderInfo> miaosha(Model model, MiaoshaUser user,
+									 @RequestParam("goodsId")long goodsId) {
+		model.addAttribute("user", user);
+		if(user == null) {
+			return Result.error(CodeMsg.SESSION_ERROR);
+		}
+		//判断库存
+		GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);//10个商品，req1 req2
+		int stock = goods.getStockCount();
+		if(stock <= 0) {
+			return Result.error(CodeMsg.MIAO_SHA_OVER);
+		}
+		//判断是否已经秒杀到了
+		MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+		if(order != null) {
+			return Result.error(CodeMsg.REPEATE_MIAOSHA);
+		}
+		//减库存 下订单 写入秒杀订单
+		OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
+		return Result.success(orderInfo);
+	}
 }
